9439434d6f65f7a9e653a3527da2d1b8b5e16a74
falguni <pfalguni307@gmail.com>
2018-05-13 22:22:35 +0530

    Pinterest Copy Layout

---
+++ app/src/main/java/com/example/hp/pinterestcopylayoutchallenge/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.hp.pinterestcopylayoutchallenge;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
+
+    }
+
+}
